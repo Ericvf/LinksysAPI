64caa0da533999c6cfc8705e3d8bd5065e0b736e
Fex <e.feggelen@outlook.com>
2017-06-25 15:27:03 +0200

    Add project files.

---
+++ LinksysApi/LinksysApi.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace LinksysApi
+{
+    public class LinksysApi
+    {
+        public class TcpClient
+        {
+            public string Expire { get; set; }
+            public string IP { get; set; }
+            public string Mac { get; set; }
+            public string Name { get; set; }
+            public string Type { get; set; }
+            public string Strength { get; set; }
+        }
+
+        public static IEnumerable<TcpClient> GetClients(string hostname, string username, string password)
+        {
+            using (var client = new WebClient())
+            {
+                client.Credentials = new NetworkCredential(username, password);
+
+                var clientPageUrl = string.Format("http://{0}/DHCPTable.asp", hostname);
+                var input = client.DownloadString(clientPageUrl);
+
+                //table[0] = new AAA('192-168-1-102', '192.168.1.102', '00:26:18:AA:28:01', '16:01:45', 'LAN');
+                var matches = Regex.Matches(input, @"^table(.*)\('(.*)','(.*)','(.*)','(.*)','(.*)'\);$", RegexOptions.Multiline);
+
+                return from match in matches.Cast<Match>()
+                       select new TcpClient
+                       {
+                           Name = match.Groups[2].Value,
+                           IP = match.Groups[3].Value,
+                           Mac = match.Groups[4].Value,
+                           Expire = match.Groups[5].Value,
+                           Type = match.Groups[6].Value,
+                       };
+            }
+        }
+    }
+}
---
+++ LinksysApi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace LinksysApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                var clients = LinksysApi.GetClients("192.168.1.1", "admin", "admin");
+
+                clients = clients.OrderBy(c => c.Type).ThenBy(c => c.Name);
+                foreach (var client in clients)
+                {
+                    Console.WriteLine("{0, -30}\t{1}\t{2}\t{3}",
+                        client.Name, client.IP, client.Expire, client.Type);
+                }
+
+                Console.WriteLine();
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
